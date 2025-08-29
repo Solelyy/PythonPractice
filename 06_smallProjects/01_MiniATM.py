2d078d835c7c19fb2a82f9ec0049be8904e78400
Jessa Gozun <jessanazarenedietagozun@gmail.com>
2025-08-30 00:36:59 +0800

    Add Mini ATM Simulator exercise

--- 06_smallProjects/01_MiniATM.py
+++ 06_smallProjects/01_MiniATM.py
@@ -18,6 +18,7 @@ if the pin is wrong keep asking (max 3 attempts -> then exit),
 pin_code= 1234
 attempt= 0
 max_attempt= 3
+logged_in= False
 
 user_balance = 0
 
@@ -25,39 +26,45 @@ while attempt < max_attempt:
     user_pin= int(input("Enter PIN: "))
     if user_pin == pin_code:
         print("Welcome!")
-        
-        while True:
-            print("\n===== ATM MENU =====")
-            print("1. Check balance \n2. Deposit Money \n3. Withdraw \n4. Exit")
-            user_choice= int(input("Enter choice [1-4]: "))
-            
-            match user_choice:
-                case 1:
-                    print(f"Balance: {user_balance: .2f}")
-                    continue
-                case 2:
-                    deposit= float(input("Deposit amount in Php: "))
-                    user_balance += deposit
-                    print(f"New Balance: ₱{user_balance:,.2f}")
-                    continue
-                case 3:
-                    print(f"Balance: {user_balance: .2f}")
-                    withdrawal = float(input("Withdraw amount: "))
-                    if withdrawal <= user_balance:
-                        user_balance -= withdrawal
-                        print(f"New balance: ₱{user_balance:,.2f}")
-                        continue
-                    else:
-                        print("Insufficient balance!")
-                        continue
-                case 4:
-                    print("Thank you! Exiting...")
-                    exit()
-                case _:
-                    print("Enter between 1 and 4 only.")
-            break
+        logged_in= True
+        break
     else:
         attempt +=1
         print(f"Incorrect PIN. Attempts left: {max_attempt- attempt}")
+
+if logged_in:
+    while True:
+        print("\n===== ATM MENU =====")
+        print("1. Check balance \n2. Deposit Money \n3. Withdraw \n4. Exit")
+        user_choice= int(input("Enter choice [1-4]: "))
+            
+        match user_choice:
+            case 1:
+                print(f"Balance: {user_balance: .2f}")
+                continue
+            case 2:
+                deposit= float(input("Deposit amount: "))
+                if deposit > 0:
+                    user_balance += deposit
+                    print(f"New Balance: ₱{user_balance:,.2f}")
+                else:
+                    print("Invalid deposit amount!")
+                continue
+            case 3:
+                print(f"Balance: {user_balance: .2f}")
+                withdrawal = float(input("Withdraw amount: "))
+                if withdrawal <= user_balance:
+                    user_balance -= withdrawal
+                    print(f"New balance: ₱{user_balance:,.2f}")
+                    continue
+                else:
+                    print("Insufficient balance!")
+                    continue
+            case 4:
+                print("Thank you! Exiting...")
+                break
+            case _:
+                print("Enter between 1 and 4 only.")
+        break
 else:
     print("Too many attempts! Access denied.")
